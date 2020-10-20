f851f5e56be04745d295099ca81888f7468ffece
greatwallet <cxt_tsinghua@126.com>
2020-10-20 13:29:21 +0800

    add time count

--- TopSort/main.cpp
+++ TopSort/main.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
+#include <iomanip>
+#include <chrono>
 #include "DiGraph.h"
 using namespace std;
 
 #define FILE_NAME "DAG.txt"
 int main()
 {
+	// timer
+	chrono::time_point<chrono::steady_clock> tic;
+	chrono::time_point<chrono::steady_clock> toc;
+	double elapsed_time;
+
 	// Create a graph given in the above diagram 
-	//DiGraph g(6);
-	//g.addEdge(5, 2);
-	//g.addEdge(5, 0);
-	//g.addEdge(4, 0);
-	//g.addEdge(4, 1);
-	//g.addEdge(2, 3);
-	//g.addEdge(3, 1);
 	DiGraph g(FILE_NAME);
 
-
+	// algorithm recursively
 	cout << "All Topological sorts Recursively\n";
-
+	tic = chrono::high_resolution_clock::now();
 	g.alltopologicalSortRecursive();
+	toc = chrono::high_resolution_clock::now();
+	elapsed_time = chrono::duration<double>(toc - tic).count();
+	cout << "[Recursive] Time Cost = " << setprecision(2) << elapsed_time << "s" << endl;
 
+	// algorithm K
 	cout << "All Topological sorts_K\n";
 	list<list<int>> sort_list;
+
+	tic = chrono::high_resolution_clock::now();
 	int code = g.all_topological_sorts_K(sort_list);
 	if (code != 0)
 	{
 		cout << "Error taking place in top_sort_K" << endl;
 		return -1;
 	}
-	for (auto const& sort : sort_list) {
-		for (auto const& e : sort) 
-			cout << e << " ";
-		cout << endl;
-	}
+	//for (auto const& sort : sort_list) {
+	//	for (auto const& e : sort) 
+	//		cout << e << " ";
+	//	cout << endl;
+	//}
+	toc = std::chrono::high_resolution_clock::now();
+	elapsed_time = chrono::duration<double>(toc - tic).count();
+	cout << "[K] Time Cost = " << setprecision(2) << elapsed_time << "s" << endl;
+
 	return 0;
 }
